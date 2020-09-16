1f82bd4f39664a1f25d597460a4c49d162c5d282
Krisz <krisz@example.com>
2020-09-16 20:18:16 +0200

    Updates

---
+++ precache-manifest.9f3fd057060b127a0d3cc69784dc6f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0486ceb322897f63db547d0c0a32b0f9",
+    "url": "/Word-Associative-Quiz-Game/index.html"
+  },
+  {
+    "revision": "4540e1805a949b352b25",
+    "url": "/Word-Associative-Quiz-Game/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "e98822b6d17db38bc517",
+    "url": "/Word-Associative-Quiz-Game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4540e1805a949b352b25",
+    "url": "/Word-Associative-Quiz-Game/static/js/2.0c391e99.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/Word-Associative-Quiz-Game/static/js/2.0c391e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98822b6d17db38bc517",
+    "url": "/Word-Associative-Quiz-Game/static/js/main.e19cb66b.chunk.js"
+  },
+  {
+    "revision": "675887bacd5c1bf36a0a",
+    "url": "/Word-Associative-Quiz-Game/static/js/runtime-main.2d73c243.js"
+  }
+]);
