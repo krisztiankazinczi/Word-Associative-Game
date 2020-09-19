b252ac40d0c916fb087afea7f12ce2344a614be5
Krisz <krisz@example.com>
2020-09-19 17:16:10 +0200

    axios baseURL changed

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3030/api/v1",
-  // baseURL: "https://word-associative-game-back-end.herokuapp.com/api/v1",
+  // baseURL: "http://localhost:3030/api/v1",
+  baseURL: "https://word-associative-game-back-end.herokuapp.com/api/v1",
 });
 
 export default instance;
